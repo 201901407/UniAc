44586bdab4c4f68d05dc086ec0aed68b57664bb5
Darshil Parikh <201901407@daiict.ac.in>
2022-11-18 22:16:03 +0530

    Update settings.py

--- accreditation_system/settings.py
+++ accreditation_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f=94re$@7pmo=3amh93x6%((5qqr0^7ux^=g*vtb_g)03#8s(('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
